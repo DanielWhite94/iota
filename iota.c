c876324862c428ca580f6632cd76e4c85451eb03
Daniel White <DanielWhite94@server.fake>
2015-02-02 17:50:09 +0000

    Defined macro Z as ';if(!strncmp(c,' saving 19 bytes.

--- iota.c
+++ iota.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#define Z ;if(!strncmp(c,
+
 main() {
 	char c[99999];
   for(;;fflush(stdout)) { // Loop and ensure any data reaches the GUI.
-  	strtok(gets(c), " "); // Wait for a command.
-  	if(!strncmp(c,"uci",4)) puts("id name i\nid author DJW\nuciok"); // Reply to 'uci'.
-  	if(!strncmp(c,"i",1)) puts("readyok"); // Reply to 'isready'.
-  	if(!strncmp(c,"q",1)) break; // Quit command, exit.
-}}
+  	strtok(gets(c), " ") // Wait for a command.
+  	Z"uci",4)) puts("id name i\nid author DJW\nuciok") // Reply to 'uci'.
+  	Z"i",1)) puts("readyok") // Reply to 'isready'.
+  	Z"q",1)) break; // Quit command, exit.
+  }
+}
