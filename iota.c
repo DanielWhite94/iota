bef728ea4446daa7927b433b79194f64f084d1b7
Daniel White <DanielWhite94@server.fake>
2015-02-05 20:16:16 +0000

    Now support en-passent captures.

--- iota.c
+++ iota.c
@@ -6,10 +6,10 @@
 
 char c[Q], *C,
      *T="#6*.683234#15BD#24#24$3#(.)$?).(/.2##$"; // Piece move steps, board setup data and promotion XORs (subtract '#'=35).
-B[Q], S, W, X;
+B[Q], R, S, W, X;
 
-F(U, V, p) {
-	int f=0,t, i, d, P, u;
+F(U, V, p, r) {
+	int f=0,t, i, d, P, u, q;
 	for(;f<128;f=f+9&~8) { // Loop over squares looking for pieces.
 		for(i=T[(P=B[f])&7]-35,d=0;d<0 || (d=T[i++]-35);d=-d) { // Loop over move steps for this piece.
 			for(t=f+d;P&S;t+=d) { // Loop over destination squares in this direction.
@@ -17,18 +17,28 @@ F(U, V, p) {
 				if ((t&136) || ((u=B[t])&S))
 					break;
 
-				// Check for correct pawn movement.
-				if (P%8==2 && ((d^(u>0))%2 || ((d>0)^(S==32))))
-					break;
+				// Special pawn logic.
+				q=128;
+				if (P%8==2)
+				{
+					if (((d>0)^(S==32)) || // Bad direction?
+							(!u && d%2 && t!=r) || // Diagonal without capture?
+							(!(d%2) && u>0)) // Straight with capture?
+						break;
+
+					q=(t==r ? 16 : q); // Is this an en-passent capture?
+				}
 
 				// Other side left in check?
 				if (U==8 && (u&16))
 					return 0;
+
 				// Make move.
 				B[t]=P;
+				B[f]=0;
+				B[t^q]=0; // If en-passent capture, remove victim pawn.
 				if (P%8==2 && (t<8 || t>103))
 					B[t]^=T[p%7+32]-35; // Promotion.
-				B[f]=0;
 				S^=96;
 				if ((P&16) && t!=f+d)
 					B[(f+t)/2]=B[t+(d>0?1:-2)],B[t+(d>0?1:-2)]=0; // If castling also move rook.
@@ -36,10 +46,12 @@ F(U, V, p) {
 				// Looking to make a move? (if our own move, make sure does not leave us in check)
 				if ((t==V && f==U) || (U==Q && F(8,0,0,Q))) {
 					W=f,X=t;
+					R=((P%8==2 && t!=f+d) ? (t+f)/2 : 9); // Set ep-target square if double pawn move.
 					return B[t]==P; // Indicate if promotion has NOT occured.
 				}
 
 				// Undo move.
+				B[t^q]=10|S; // If en-passent capture, replace victim pawn.
 				S^=96;
 				B[f]=P;
 				B[t]=u;
@@ -68,14 +80,14 @@ main(i, p) {
 		Z"q",1)) break; // Quit command, exit.
 		Z"p",1)) { // Parse 'position' command.
 			for(i=0;i<8;++i) // Reset board to start position.
-				B[i+16]=42,B[i+96]=74, // Pawns.
+				B[i+16]=R=42,B[i+96]=74, // Pawns.
 				B[i+112]=(B[i]=T[i+24]-3)+(S=32), // Pieces.
 				B[i+S]=B[i+48]=B[i+64]=B[i+80]=0; // Empty rows.
 			for(;C=strtok(0," ");)
-				F(*C+C[1]*16-881, C[2]+C[3]*16-881, C[4]);
+				F(*C+C[1]*16-881, C[2]+C[3]*16-881, C[4], R);
 		}
 		Z"g",1)) // Go command. Make a move and print it.
-			p=F(Q, 0, 0),
+			p=F(Q, 0, 0, R),
 			printf("bestmove %c%i%c%i%c\n", W%16+97, W/16+1, X%16+97, X/16+1, p?32:98);
 	}
 }
