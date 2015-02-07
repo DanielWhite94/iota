e945c7a38f675d46da8abb7662a19369d3f1c545
Daniel White <DanielWhite94@server.fake>
2015-02-07 14:52:29 +0000

    Renamed many symbols in an attempt to improve readability.

--- iota.c
+++ iota.c
@@ -1,67 +1,67 @@
 #include <stdio.h>
 
 #define I if(
-#define M )break;
-#define N for(
-#define Q 99999
+#define K )break;
+#define F for(
+#define H 99999
 #define Z ;if(!strncmp(c,
 
-char c[Q], *C,
+char c[H], *C,
      *T="#5)-57234#15BD#24#24$3#EKFA\\FKE/.2##$"; // Piece move steps, board setup data and promotion XORs (subtract '#'=35).
 
-B[Q], R, S, W, X;
+B[H], E, S, X, Y;
 
-F(U, V, p, r) {
-	int f=0,t, i, d, P, u, q;
-	N ;f<128;f=f+9&~8) { // Loop over squares looking for pieces.
-		N i=T[(P=B[f])&7]-35,d=0;d<0 || (d=T[i++]-35);d=-d) { // Loop over move steps for this piece.
-			N t=f;!((t+=d)&(q=136)) && P&S;) { // Loop over destination squares in this direction.
+D(x, y, p, e) {
+	int f=0,t, i, d, m, v, b;
+	F ;f<128;f=f+9&~8) { // Loop over squares looking for pieces.
+		F i=T[(m=B[f])&7]-35,d=0;d<0 || (d=T[i++]-35);d=-d) { // Loop over move steps for this piece.
+			F t=f;!((t+=d)&(b=136)) && m&S;) { // Loop over destination squares in this direction.
 				// Friendly capture?
-				I (u=B[t])&S M
+				I (v=B[t])&S K
 
 				// Special pawn logic.
-				I P%8==2)
+				I m%8==2)
 				{
 					I ((d>0)^(S==32)) || // Bad direction?
-					   (!u && d%2 && t!=r) | // Diagonal without capture?
-					   (!(d%2) && u>0) // Straight with capture?
-						M
+					   (!v && d%2 && t!=e) | // Diagonal without capture?
+					   (!(d%2) && v>0) // Straight with capture?
+						K
 
-					I t==r) q=t^16; // Is this an en-passent capture?
+					I t==e) b=t^16; // Is this an en-passent capture?
 				}
 
 				// Other side left in check?
-				I U==8 && u&16)
+				I x==8 && v&16)
 					return 0;
 
 				// Make move.
-				B[t]=P%128;
-				B[f]=B[q]=0; // If en-passent capture, remove victim pawn.
-				I P%8==2 && (t<8 || t>103))
+				B[t]=m%128;
+				B[f]=B[b]=0; // If en-passent capture, remove victim pawn.
+				I m%8==2 && (t<8 || t>103))
 					B[t]^=T[p%7+31]-35; // Promotion.
 				S^=96;
-				I P&16 && t!=f+d)
+				I m&16 && t!=f+d)
 					B[f+t>>1]=B[t+(d*3-1)/2],B[t+(d*3-1)/2]=0; // If castling also move rook.
 
 				// Looking to make a move? (if our own move, make sure does not leave us in check)
-				I (t==V && f==U) | (U==Q && F(8,0,0,Q))) {
-					W=f,X=t;
-					R=(P%8==2 && t!=f+d ? f+t>>1 : 9); // Set ep-target square if double pawn move.
-					return B[t]==P%128; // Indicate if promotion has NOT occured.
+				I (t==y && f==x) | (x==H && D(8,0,0,H))) {
+					X=f,Y=t;
+					E=(m%8==2 && t!=f+d ? f+t>>1 : 9); // Set ep-target square if double pawn move.
+					return B[t]==m%128; // Indicate if promotion has NOT occured.
 				}
 
 				// Undo move.
-				B[q]=10|S; // If en-passent capture, replace victim pawn.
+				B[b]=10|S; // If en-passent capture, replace victim pawn.
 				S^=96;
-				B[f]=P;
-				B[t]=u;
+				B[f]=m;
+				B[t]=v;
 
-				I (P&16 && f==U && t+d==V) | // If GUI has given us a castling move,
-				   (P&128 && t==f+d && !(d%2))) // or double pawn first move, loop once more.
+				I (m&16 && f==x && t+d==y) | // If GUI has given us a castling move,
+				   (m&128 && t==f+d && !(d%2))) // or double pawn first move, loop once more.
 					continue;
 				
 				// Hit a piece or non-slider?
-				I u || P&8 M
+				I v || m&8 K
 			}
 		}
 	}
@@ -69,19 +69,19 @@ F(U, V, p, r) {
 }
 
 main(i) {
-	N ;strtok(gets(c)," ");fflush(stdout)) { // Loop, grabbing input and flushing output.
+	F ;strtok(gets(c)," ");fflush(stdout)) { // Loop, grabbing input and flushing output.
 		Z"uci",4)) puts("id name i\nid author D\nuciok") // Reply to 'uci'.
 		Z"i",1)) puts("readyok") // Reply to 'isready'.
 		Z"p",1)) { // Parse 'position' command.
-			N i=0;i<8;++i) // Reset board to start position.
-				B[i+16]=R=170,B[i+96]=202, // Pawns.
+			F i=0;i<8;++i) // Reset board to start position.
+				B[i+16]=E=170,B[i+96]=202, // Pawns.
 				B[i]=(B[i+112]=T[i+23])-(S=32), // Pieces.
 				B[i+S]=B[i+48]=B[i+64]=B[i+80]=0; // Empty rows.
-			N ;C=strtok(0," ");)
-				F(*C+C[1]*16-881, C[2]+C[3]*16-881, C[4], R);
+			F ;C=strtok(0," ");)
+				D(*C+C[1]*16-881, C[2]+C[3]*16-881, C[4], E);
 		}
 		Z"g",1)) // Go command. Make a move and print it.
-			i=F(Q, 0, 0, R),
-			printf("bestmove %c%i%c%i%c\n", W%16+97, W/16+1, X%16+97, X/16+1, i?32:98);
+			i=D(H, 0, 0, E),
+			printf("bestmove %c%i%c%i%c\n", X%16+97, X/16+1, Y%16+97, Y/16+1, i?32:98);
 	}
 }
